35f2344f0ddeace4426334cf021a1442dc35350a
Augustin JOUAN <augustin.jouan@epitech.eu>
2025-10-26 22:44:36 +0100

    feat(lindex):links are available fix(inex):links & form are responsive

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const btn = document.getElementById("btn");
 const form = document.getElementById("form");
 btn.addEventListener("click", () => {
-    form.classList.toggle('translate-x-[120%]');
+    form.classList.toggle('translate-x-[140%]');
 
 })
 
@@ -12,11 +12,11 @@ btn.addEventListener("click", () => {
 const btn2 = document.getElementById("btn2");
 const link = document.getElementById("links");
 btn2.addEventListener("click", () => {
-    link.classList.toggle('translate-y-[120%]');
+    link.classList.toggle('translate-y-[130%]');
 
 })
 
-// ici je gère les botons pour afficher/enlever les grandes cartes
+// ici je gère les botons pour afficher/enlever les cards de ma bio
 const expandButtons = document.querySelectorAll('.expand-btn');
 const backButtons = document.querySelectorAll('.back-btn');
 const smallCards = document.querySelectorAll('.card');
@@ -34,5 +34,27 @@ backButtons.forEach(btn => {
   btn.addEventListener('click', () => {
     bigCards.forEach(c => c.classList.add('hidden')); // cache toutes les grandes
     smallCards.forEach(c => c.classList.remove('hidden')); // réaffiche les petites
+  });
+});
+
+// ici je gère les botons pour afficher/enlever les cards de mes projets
+
+const projectBtns = document.querySelectorAll('.project-btn');
+const backProjectBtns = document.querySelectorAll('.back-project');
+const smallProjects = document.querySelectorAll('.project-card');
+const bigProjects = document.querySelectorAll('.big-project');
+
+projectBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const target = btn.dataset.target;
+    smallProjects.forEach(p => p.classList.add('hidden'));
+    document.getElementById(`big-${target}`).classList.remove('hidden');
+  });
+});
+
+backProjectBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    bigProjects.forEach(p => p.classList.add('hidden'));
+    smallProjects.forEach(p => p.classList.remove('hidden'));
   });
 });
